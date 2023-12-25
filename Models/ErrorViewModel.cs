89fa3fe8a0e8095edbaada9e711bdecdb1910d57
Adekunle Afolabi <32042328+RHeX700@users.noreply.github.com>
2023-12-25 10:20:01 +0100

    Initialization

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Real_Time_Charts_SignalR.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
